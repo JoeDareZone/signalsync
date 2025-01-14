9b96ee2d9eda7dcb0376df1db0697377b0c7a551
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-14 10:33:23 +0100

    feat: :sparkles: Add dot indicators and intro screens

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    PageController _controller = PageController();
+
     return Scaffold(
-      body: PageView(
-        children: [
-          Container(color: Colors.blue[800],),
-          Container(color: Colors.yellow[800],),
-          Container(color: Colors.green[800],),
-        ]
-      )
-    );
+        body: Stack(children: [
+      PageView(controller: _controller, children: [
+        Container(
+          color: Colors.blue[800],
+        ),
+        Container(
+          color: Colors.yellow[800],
+        ),
+        Container(
+          color: Colors.green[800],
+        ),
+      ]),
+      Container(alignment: Alignment(0, 0.75),
+        child: SmoothPageIndicator(controller: _controller, count: 3))
+    ]));
   }
 }
