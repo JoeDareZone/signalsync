8a63b75b984edee4705c589a3597d4fa41602cb8
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-14 10:50:41 +0100

    feat: :sparkles: Add next and done buttons

--- lib/onboarding_screen.dart
+++ lib/onboarding_screen.dart
@@ -4,23 +4,58 @@ import 'package:signalsync/intro_screens/intro_page_2.dart';
 import 'package:signalsync/intro_screens/intro_page_3.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-class OnboardingScreen extends StatelessWidget {
+class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    PageController _controller = PageController();
+  State<OnboardingScreen> createState() => _OnboardingScreenState();
+}
 
+class _OnboardingScreenState extends State<OnboardingScreen> {
+  PageController _controller = PageController();
+  bool isLastPage = false;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: Stack(children: [
-      PageView(controller: _controller, children: [
-        IntroPage1(),
-        IntroPage2(),
-        IntroPage3(),
-      ]),
+      PageView(
+          controller: _controller,
+          onPageChanged: (index) => {
+                setState(() {
+                  isLastPage = (index == 2);
+                })
+              },
+          children: [
+            IntroPage1(),
+            IntroPage2(),
+            IntroPage3(),
+          ]),
       Container(
           alignment: Alignment(0, 0.75),
-          child: SmoothPageIndicator(controller: _controller, count: 3))
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () => _controller.jumpToPage(2),
+                child: Text('skip'),
+              ),
+              SmoothPageIndicator(controller: _controller, count: 3),
+              isLastPage
+                  ? GestureDetector(
+                      onTap: () => _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn),
+                      child: Text('done'),
+                    )
+                  : GestureDetector(
+                      onTap: () => _controller.nextPage(
+                          duration: Duration(milliseconds: 500),
+                          curve: Curves.easeIn),
+                      child: Text('next'),
+                    )
+            ],
+          ))
     ]));
   }
 }
