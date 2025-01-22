b08d79a15d47523955aca40c10cafd345426242e
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-22 15:23:35 +0100

    fix: :bug: Navigate user to welcome page after logging out

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,28 +1,33 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:signalsync/screens/auth/welcome_page.dart'; // Adjust the import path
 
 class HomePage extends StatelessWidget {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
   HomePage({super.key});
 
-  Future<void> _signOut() async {
+  Future<void> _signOut(BuildContext context) async {
     await _auth.signOut();
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => WelcomePage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Screen'),
+        title: const Text('Home Screen'),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout),
-            onPressed: _signOut,
+            icon: const Icon(Icons.logout),
+            onPressed: () => _signOut(context),
           ),
         ],
       ),
-      body: Center(
+      body: const Center(
         child: Text('Welcome to the Home Screen!'),
       ),
     );
